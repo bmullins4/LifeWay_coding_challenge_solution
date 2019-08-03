1c718d85953d32d182b9036c256f947491a5f533
Ben Mullins <bmullins4@murraystate.edu>
2019-08-02 23:20:35 -0500

    Add Main

---
+++ src/main/java/io/github/bmullins4/Main.java
@@ -0,0 +1,14 @@
+
+package io.github.bmullins4;
+
+/**
+ *
+ * @author Benjamin Mullins
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+    }
+}
